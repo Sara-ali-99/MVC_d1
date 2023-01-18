a0382be6e4569dd0847a393a57d6df49f228c1e6
sara <sara@DESKTOP-GKI32UC>
2023-01-18 20:05:20 +0200

    Add project files.

---
+++ MVC_d1/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_d1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        { 
+            return View ("details");
+          
+        }
+      
+    }
+}
---
+++ MVC_d1/Models/user.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_d1.Models
+{
+    public class user
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+
+        public string willAttend { get; set; }
+    }
+}
